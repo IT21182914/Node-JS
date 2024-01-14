47e34bdc7d105b1c7c96e456cb30389f235a2375
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-15 05:25:30 +0530

    Fs Async

--- modules/built-in-modules/fs-async.js
+++ modules/built-in-modules/fs-async.js
@@ -1,5 +1,7 @@
 const { readFile, writeFile } = require("fs"); // fs is file system module in node.js
 
+//destructuring readFile and writeFile from fs module.destructuring is used to extract properties from objects and bind them to variables.that means we can extract properties from objects and bind them to variables.in above line we are extracting readFile and writeFile from fs module and binding them to variables.Otherwise we can use it as const fs = require('fs'); fs.readFile(); fs.writeFile();.But we are using destructuring here.
+
 console.log("Start");
 readFile(
   "./modules/built-in-modules/content/first.txt",
