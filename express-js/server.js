aefaa91dcc74b873741b7b00600920534db03cb6
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-18 08:18:32 +0530

    Reqest & Respond

--- express-js/server.js
+++ express-js/server.js
@@ -1,1 +1,8 @@
-console.log("Express JS Tutorial");
+const http = require("http");
+
+// this is for the server to listen to the request and respond to it with a callback function that takes in two parameters req and res which are the request and response objects respectively and the callback function is executed every time a request is made to the server and the server responds to it
+const server = http.createServer((req, res) => {
+  console.log("user hit the server");
+});
+
+server.listen(5000);
