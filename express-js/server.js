93cc74bb291e12fa5d662b0a15243c657fac097b
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-21 13:40:05 +0530

    Express JS

---
+++ express-js/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = 8080;
+
+app.listen(PORT, () => console.log(`\nServer running on port ${PORT} 🔥`));
+
+//app.get
+//app.post
+//app.put
+//app.delete
+//app.all
+//app.use
+//app.listen
