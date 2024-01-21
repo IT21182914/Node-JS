15d141eaecb8f672a40797a037ca98960ed0fffb
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-21 09:30:50 +0530

    Express Server

--- express-js/server.js
+++ express-js/server.js
@@ -1,6 +1,7 @@
-const http = require("http");
+const http = require("http"); //http is a built-in module in Node JS. we don't need to install it.
 
 //server to listen to the request and respond to it with a callback function that takes in two parameters
+
 const server = http.createServer((req, res) => {
   console.log("user hit the server");
 });
