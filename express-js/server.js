7730080aa2eb87e240e65d1c9b50194119ca848d
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-25 16:23:21 +0530

    Postman POST

--- express-js/server.js
+++ express-js/server.js
@@ -21,6 +21,16 @@ app.post("/api/people", (req, res) => {
   res.status(201).json({ success: true, person: name });
 });
 
+app.post("/api/postman/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "Please provide a name value" });
+  }
+  res.status(201).json({ success: true, data: [...people, name] });
+});
+
 app.post("/login", (req, res) => {
   const { name } = req.body;
   if (name) {
