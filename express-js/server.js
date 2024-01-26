dd066e619257f286301332a088b06388185e7dc7
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-26 14:59:48 +0530

    PUT

--- express-js/server.js
+++ express-js/server.js
@@ -43,8 +43,19 @@ app.put("/api/people/:id", (req, res) => {
   const { id } = req.params;
   const { name } = req.body;
 
-  console.log(id, name);
-  res.send("hello world");
+  const person = people.find((person) => person.id === Number(id));
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msg: `No person with id ${id}` });
+  }
+  const newPeople = people.map((person) => {
+    if (person.id === Number(id)) {
+      person.name = name;
+    }
+    return person;
+  });
+  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.listen(PORT, () => console.log(`\nServer is running on port ${PORT} 🔥`));
